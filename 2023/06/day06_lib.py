73ea1468142863105ee003dbca253fb2eb697700
Alicja Kozikowska <alicja.kozikowska@thermondo.de>
2024-07-28 16:52:34 +0200

    day 6: part 2 + refactor

--- 2023/06/day06_lib.py
+++ 2023/06/day06_lib.py
@@ -1,19 +1,30 @@
-def get_ways_to_win_multiplied(test_input: str):
-    time_line, distance_line = test_input.splitlines(keepends=False)
+def get_ways_to_win_one_race(time: int, distance: int) -> int:
+    num_ways_to_win = 0
+
+    for button_press_time in range(1, time):
+        speed = button_press_time
+        remaining_time = time - button_press_time
+        distance_traveled = speed * remaining_time
+        if distance_traveled > distance:
+            num_ways_to_win += 1
+
+    return num_ways_to_win
+
+
+def get_ways_to_win_multiplied(race_input: str) -> int:
+    time_line, distance_line = race_input.splitlines(keepends=False)
     times = [int(t) for t in time_line.split()[1:]]
     distances = [int(d) for d in distance_line.split()[1:]]
     num_ways_to_win_multiplied = 1
 
     for time, distance in zip(times, distances):
-        num_ways_to_win = 0
-
-        for button_press_time in range(1, time):
-            speed = button_press_time
-            remaining_time = time - button_press_time
-            distance_traveled = speed * remaining_time
-            if distance_traveled > distance:
-                num_ways_to_win += 1
-
-        num_ways_to_win_multiplied *= num_ways_to_win
+        num_ways_to_win_multiplied *= get_ways_to_win_one_race(time, distance)
 
     return num_ways_to_win_multiplied
+
+
+def get_ways_to_win_long_race(race_input: str) -> int:
+    time_line, distance_line = race_input.splitlines(keepends=False)
+    time = int("".join([t for t in time_line.split()[1:]]))
+    distance = int("".join([t for t in distance_line.split()[1:]]))
+    return get_ways_to_win_one_race(time, distance)
--- 2023/06/test_day06.py
+++ 2023/06/test_day06.py
@@ -1,4 +1,4 @@
-from day06_lib import get_ways_to_win_multiplied
+from day06_lib import get_ways_to_win_multiplied, get_ways_to_win_long_race
 
 TEST_INPUT = """Time:      7  15   30
 Distance:  9  40  200
@@ -7,3 +7,7 @@ Distance:  9  40  200
 
 def test_get_ways_to_win_multiplied():
     assert get_ways_to_win_multiplied(TEST_INPUT) == 288
+
+
+def test_get_ways_to_win_long_race():
+    assert get_ways_to_win_long_race(TEST_INPUT) == 71503
