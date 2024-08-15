7238839c16a6db4a1826b896522ed7545454601e
Alicja Kozikowska <alakozikowska@gmail.com>
2024-08-15 16:41:19 +0300

    day 10: solve part 1

--- 2023/10/lib.py
+++ 2023/10/lib.py
@@ -1,12 +1,56 @@
+CONNECTORS = {
+    "|": {"N", "S"},  # vertical pipe connecting north and south
+    "-": {"E", "W"},  # horizontal pipe connecting east and west
+    "L": {"N", "E"},  # 90-degree bend connecting north and east
+    "J": {"N", "W"},  # 90-degree bend connecting north and west
+    "7": {"S", "W"},  # 90-degree bend connecting south and west
+    "F": {"S", "E"},  # 90-degree bend connecting south and east
+}
+
+DIRECTIONS = {
+    "N": (-1, 0),
+    "S": (1, 0),
+    "E": (0, 1),
+    "W": (0, -1),
+}
+
+
+OPPOSITES = {"N": "S", "S": "N", "E": "W", "W": "E"}
+
+
 def part_1(input_text: str) -> int:
     matrix = input_text.splitlines()
     i, j = get_starting_point(matrix)
-    # for neighbor in [(i, j-1), (i, j+1), (i-1, j), (i+1, j)]:
-    #     new_i, new_j = neighbor
-    #     if new_i > -1 and new_j > -1:
-    #         point = matrix[new_i][new_j]
-    #         if
-    return 0
+
+    paths = []
+
+    for direction in DIRECTIONS:
+        diff_i, diff_j = DIRECTIONS[direction]
+        new_i, new_j = i + diff_i, j + diff_j
+        from_direction = OPPOSITES[direction]
+        connector = matrix[new_i][new_j]
+        if connector in CONNECTORS:
+            connector_directions = CONNECTORS[connector]
+            if from_direction in connector_directions:
+                next_direction = (connector_directions - {from_direction}).pop()
+                path = [(new_i, new_j, next_direction)]
+                paths.append(path)
+
+    steps = 1
+
+    while not paths[0][-1][:2] == paths[1][-1][:2]:
+        for path in paths:
+            i, j, direction = path[-1]
+            diff_i, diff_j = DIRECTIONS[direction]
+            new_i, new_j = i + diff_i, j + diff_j
+            from_direction = OPPOSITES[direction]
+            connector = matrix[new_i][new_j]
+            connector_directions = CONNECTORS[connector]
+            next_direction = (connector_directions - {from_direction}).pop()
+            path.append((new_i, new_j, next_direction))
+        steps += 1
+
+    return steps
 
 
 def get_starting_point(matrix):
